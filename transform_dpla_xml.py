c9352e4aa9e526af3d4e6d9d388d7ab679984be7
mroseberry <mroseberry@vcu.edu>
2018-10-30 17:54:17 -0400

    Updated rpr files and transformation script

--- transform_dpla_xml.py
+++ transform_dpla_xml.py
@@ -6,7 +6,11 @@ def main():
     col_file = input("Please enter the name of the input file, without the extension. The file must be in the input "
                      "folder. (e.g. for the input file input/collection.xml, enter 'collection') \n ---> ")
 
-    collection_name = input("Please enter the name of the collection as you would like it to display. \n ---> ")
+    output_name = input("Please enter the base file name for the output files. (e.g. rpr_20181030) \n ---> ")
+
+    # for easier testing on the same file
+    # col_file = 'newrpr103018'
+    # output_name = 'rpr-20181030'
 
     input_filepath = 'input/' + col_file + '.xml'
     fp_out = None
@@ -20,20 +24,44 @@ def main():
             url_substitutions = 0
             while line:
 
+                # replace any special characters that don't decode (? in little diamond) with a space
+                line = line.replace(u'\ufffd', ' ')
+
                 # fix issue with apostrophes
-                if "&#x27;" in line:
-                    line = line.replace("&#x27;", "'")
+                line = line.replace("&#x27;", "'")
+
+                # replace &
+                # line = line.replace("&amp;", "&")
 
                 # changes dc to dcterms
                 if line.startswith('<dc:'):
                     line = line.replace('<dc:', '<dcterms:', 1)
                     line = line.replace('</dc:', '</dcterms:')
 
+                # replace Stillimage in type field
+                if line.startswith('<dcterms:type>') and "Stillimage" in line:
+                    line = line.replace("Stillimage", "Still Image")
+
+                # replace xml alternative tag with title tag
+                if line.startswith('<dcterms:alternative>'):
+                    line = line.replace("<dcterms:alternative>", "<dcterms:title>")
+                    line = line.replace("</dcterms:alternative>", "</dcterms:title>")
+
+                # replace xml issued tag with created tag
+                if line.startswith('<dcterms:issued>'):
+                    line = line.replace("<dcterms:issued>", "<dcterms:created>")
+                    line = line.replace("</dcterms:issued>", "</dcterms:created>")
+
+                # add page to page number description
+                if re.match('<dcterms:description>(\d+)</dcterms:description>', line):
+                    page = re.search('<dcterms:description>(\d+)</dcterms:description>', line).group(1)
+                    line = re.sub('\d+', 'p. ' + page, line)
+
                 # indicates the start of a record
                 if fp_out is None or fp_out.closed:
                     if '<metadata>' in line:
                         # create and open output file
-                        output_file = 'output/' + col_file + '_transformed_' + str(count_records) + '.xml'
+                        output_file = 'output/' + output_name + '_' + str(count_records) + '.xml'
                         fp_out = open(output_file, 'w+')
                         count_records += 1
 
@@ -61,7 +89,7 @@ def main():
 
                     # this changes the rights statement
                     elif line.startswith('<dcterms:rights>'):
-                        fp_out.write("<dcterms:rights>http://rightsstatements.org/vocab/InC/1.0/</dcterms:rights>\n")
+                        fp_out.write("<dcterms:rights>http://rightsstatements.org/vocab/NoC-US/1.0/</dcterms:rights>\n")
                         rights_substitutions += 1
 
                     # changes the URL and adds thumbnail URL
@@ -72,9 +100,11 @@ def main():
                                 print("ERROR: unexpected input")
 
                             # get url and col_id string
-                            url = re.split('[<>]', line)[2]
+                            url = re.sub('<[^>]+>', '', line).strip()
                             url = url.replace("http://", "https://", 1)
-                            col_id = url.split('/')[6] + '/' + url.split('/')[7] + '/' + url.split('/')[8]
+                            if not url.split('/')[-2] == "id":
+                                print('ERROR: URL parse problem')
+                            col_id = url.split('/')[-3] + '/' + url.split('/')[-2] + '/' + url.split('/')[-1]
 
                             # write url line
                             fp_out.write("<edm:isShownAt>" + url + "</edm:isShownAt>\n")
@@ -90,26 +120,24 @@ def main():
                         line = line.replace("type", "medium")
                         fp_out.write(line)
 
-                    # expand type field
-                    elif line.startswith('<dcterms:type>') and ";" in line:
-                        if "Stillimage" in line:
-                            line = line.replace("Stillimage", "Still Image")
+                    # expand type, medium field
+                    elif ";" in line and (line.startswith('<dcterms:type>')
+                                          or line.startswith('<dcterms:medium')
+                                          or line.startswith('<dcterms:subject')):
 
+                        # swap out and then swap in special chars. ONLY WORKS IF ONLY ONE SPECIAL CHAR
+                        special_char = ''
+                        if '&amp;' in line:
+                            special_char = re.search('(&\S*;)', line).group(1)
+                            line = re.sub('&(\S*);', '#$%^', line)
+
+                        tag = re.search('<dcterms:(\w*)>', line).group(1)
                         values = re.split(';', re.sub('<[^>]+>', '', line).strip())
                         for value in values:
-                            fp_out.write("<dcterms:type>" + value.strip() + "</dcterms:type>\n")
-
-                    # expand type field
-                    elif line.startswith('<dcterms:medium>') and ";" in line:
-
-                        values = re.split(';', re.sub('<[^>]+>', '', line).strip())
-                        for value in values:
-                            fp_out.write("<dcterms:medium>" + value.strip() + "</dcterms:medium>\n")
-
-                    # fix little box in isPartOf (not sure why this started showing up!!!
-                    elif line.startswith('<dcterms:isPartOf>'):
-                        fp_out.write(
-                            '<dcterms:isPartOf>' + collection_name + '</dcterms:isPartOf>\n')
+                            # swap back in the special char
+                            if '#$%^' in value:
+                                value = value.replace('#$%^', special_char)
+                            fp_out.write("<dcterms:" + tag + ">" + value.strip() + "</dcterms:" + tag + ">\n")
 
                     # regular line
                     elif line.startswith('<dcterms:'):
